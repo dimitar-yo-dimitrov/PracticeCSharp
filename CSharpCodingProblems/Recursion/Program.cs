9320d36675da9f27b3146d5c51ca54400d8e6329
Dimitar Dimitrov <81187988+dimitar-yo-dimitrov@users.noreply.github.com>
2023-02-02 10:26:14 +0200

    Problem 03.Print numbers from n to 1 is added in Recursion

--- CSharpCodingProblems/Recursion/Program.cs
+++ CSharpCodingProblems/Recursion/Program.cs
@@ -4,26 +4,43 @@
     {
         public static void Main()
         {
-            int number;
-
             //Problem 1: Factorial of a Number using Recursion:
 
-            number = 5;
+            int numFactorial = 5;
 
-            Console.WriteLine($"The factorial of {number} is {Factorial(number)}");
+            Console.WriteLine($"The factorial of {numFactorial} is {Factorial(numFactorial)}");
+            Console.WriteLine("-----------------------------------------------------------------");
             Console.ReadKey();
 
             //Problem 2: Print the first n natural number:
 
-            number = 15;
+            int numNatural = 15;
             int startValue = 1;
 
             // Call recursive method with two parameters.	
-            PrintNatural(startValue, number);
+            PrintNatural(startValue, numNatural);
+            Console.WriteLine();
+            Console.WriteLine("-----------------------------------------------------------------");
+            Console.ReadKey();
 
             //Problem 3: Print numbers from n to 1:
 
+            int number = 10;
 
+            PrintNumbers(number);
+            Console.WriteLine();
+            Console.WriteLine("-----------------------------------------------------------------");
+            Console.ReadKey();
+        }
+
+        private static void PrintNumbers(int n)
+        {
+            if (n != 0)
+            {
+                Console.Write(" {0} ", n);
+
+                PrintNumbers(n - 1);
+            }
         }
 
         private static int PrintNatural(int startValue, int n)
@@ -40,14 +57,14 @@
             return PrintNatural(startValue + 1, n);
         }
 
-        private static int Factorial(int number)
+        private static int Factorial(int n)
         {
-            if (number == 1)
+            if (n == 1)
             {
                 return 1;
             }
 
-            return number * Factorial(number - 1);
+            return n * Factorial(n - 1);
         }
     }
 }
