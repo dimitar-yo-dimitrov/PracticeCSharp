ea5ebefd1f59a266d94a2a0464575fbafcd0a69d
Dimitar Dimitrov <81187988+dimitar-yo-dimitrov@users.noreply.github.com>
2023-02-02 16:04:22 +0200

    Problem 07: WIP

--- CSharpCodingProblems/Recursion/Program.cs
+++ CSharpCodingProblems/Recursion/Program.cs
@@ -59,6 +59,33 @@
             Console.WriteLine();
             Console.WriteLine("-----------------------------------------------------------------");
             Console.ReadKey();
+
+            //Problem 7: Print even or odd numbers in a given range:
+
+            int start = 1;
+            int end = 20;
+
+            EvenAndOddNumbers(start, end);
+            Console.WriteLine();
+            Console.WriteLine("-----------------------------------------------------------------");
+            Console.ReadKey();
+        }
+
+        private static void EvenAndOddNumbers(int start, int end)
+        {
+            if (end == 0)
+            {
+                return;
+            }
+
+            if (start % 2 != 0)
+            {
+                Console.Write(" {0} ", start);
+                EvenAndOddNumbers(++start, end);
+            }
+
+            Console.Write(" {0} ", end);
+            EvenAndOddNumbers(start, --end);
         }
 
         //Problem 6:
