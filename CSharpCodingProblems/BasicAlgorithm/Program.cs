a925f19fadcfbb0824fc4f4c05d9b7487f828f8f
Dimitar Dimitrov <81187988+dimitar-yo-dimitrov@users.noreply.github.com>
2023-02-27 12:24:07 +0200

    Problem 1: Compute the sum of the two given integer values. If the two values are the same, then return triple their sum is added in BasicAlgorithm

---
+++ CSharpCodingProblems/BasicAlgorithm/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace BasicAlgorithm
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Problem 1: Compute the sum of the two given integer values. If the two values are the same, then return triple their sum:
+
+            Console.WriteLine(SumOfTheTwoValues(1, 2));
+            Console.WriteLine(SumOfTheTwoValues(2, 2));
+            Console.WriteLine(SumOfTheTwoValues(5, 2));
+            Console.WriteLine("-----------------------------------------------------------------");
+            Console.ReadKey();
+        }
+
+        private static int SumOfTheTwoValues(int x, int y)
+        {
+            return x == y ? (x + y) * 3 : x + y;
+        }
+    }
+}
