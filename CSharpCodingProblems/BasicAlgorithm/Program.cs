b84c0b352442ccadf12bf6904685114f833c7a9f
Dimitar Dimitrov <81187988+dimitar-yo-dimitrov@users.noreply.github.com>
2023-02-27 12:42:18 +0200

    Problem 2: Get the absolute difference between n and 51. If n is greater than 51 return triple the absolute difference is added in BasicAlgorithm

--- CSharpCodingProblems/BasicAlgorithm/Program.cs
+++ CSharpCodingProblems/BasicAlgorithm/Program.cs
@@ -11,6 +11,26 @@
             Console.WriteLine(SumOfTheTwoValues(5, 2));
             Console.WriteLine("-----------------------------------------------------------------");
             Console.ReadKey();
+
+            //Problem 2: Get the absolute difference between n and 51. If n is greater than 51 return triple the absolute difference:
+
+            Console.WriteLine(TakeDifference(47));
+            Console.WriteLine(TakeDifference(51));
+            Console.WriteLine(TakeDifference(55));
+            Console.WriteLine("-----------------------------------------------------------------");
+            Console.ReadKey();
+        }
+
+        private static int TakeDifference(int n)
+        {
+            const int levelNum = 51;
+
+            if (n > levelNum)
+            {
+                return (n - levelNum) * 3;
+            }
+
+            return levelNum - n;
         }
 
         private static int SumOfTheTwoValues(int x, int y)
